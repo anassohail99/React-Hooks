7e9f8c2a8417b77415d0fd0b83f401058841b72b
anassohail99 <anas.sohailabs1@gmail.com>
2020-05-03 22:57:45 +0500

    Main JavaScript  File

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+// import HooksCounter from "./Components/UseState/HooksCounter";
+// import PreviousState from "./Components/UseState/PreviousState";
+import StateWithArrays from "./Components/UseState/StateWithArrays";
+
+function App() {
+  return (
+    <div className="App">
+      {/* <HooksCounter /> */}
+      {/* <PreviousState /> */}
+      <StateWithArrays />
+    </div>
+  );
+}
+
+export default App;
