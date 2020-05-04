c85acfe559ba93707eca53e984c507784760d52e
anassohail99 <anas.sohailabs1@gmail.com>
2020-05-04 23:05:32 +0500

    updated App.js File

--- src/App.js
+++ src/App.js
@@ -3,13 +3,30 @@ import "./App.css";
 // import HooksCounter from "./Components/UseState/HooksCounter";
 // import PreviousState from "./Components/UseState/PreviousState";
 import StateWithArrays from "./Components/UseState/StateWithArrays";
+import ClassCounterOne from "./Components/UseEffect/ClassCounterOne";
+import ClassCounterOneHooks from "./Components/UseEffect/ClassCounterOne(Hooks)";
+import ConditionRunEffect from "./Components/UseEffect/ConditionRunEffect";
+import ClassMouse from "./Components/UseEffect/ClassMouse";
+import MouseContainer from "./Components/UseEffect/MouseContainer";
+import IntervalClassCounter from "./Components/UseEffect/IntervalClassCounter";
+import IntervalHooksCounter from "./Components/UseEffect/IntervalHooksCounter";
+import FetchingDataWithuseEffect from "./Components/UseEffect/Fetching Data/FetchingDataWithuseEffect";
 
 function App() {
   return (
     <div className="App">
       {/* <HooksCounter /> */}
       {/* <PreviousState /> */}
-      <StateWithArrays />
+      {/* <StateWithArrays /> */}
+      {/* <ClassCounterOne /> */}
+      {/* <ClassCounterOneHooks /> */}
+      {/* <ConditionRunEffect /> */}
+      {/* <ClassMouse /> */}
+      {/* <HooksMouse /> */}
+      {/* <MouseContainer /> */}
+      {/* <IntervalClassCounter />
+      <IntervalHooksCounter /> */}
+      <FetchingDataWithuseEffect />
     </div>
   );
 }
